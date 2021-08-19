1c95781ed999eee856a9f12ae602f312e8ef9604
sohailali26 <sohailali26011999@gmail.com>
2021-08-19 20:01:40 +0530

    Wcat V1

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-const { CONNREFUSED } = require("dns");
+#!usr/bin/env node
+
 const fs=require ("fs");
-let arguents=process.argv.slice(2);
+let arguents=process.argv.slice(2); //process.argv is used to get all the arguments passed.
 console.log(arguents);
 
 let flags=[];
 let filenames=[];
+let secondaryArguments=[];
 
 for(let i of arguents){
     if(i[0]=="-"){
         flags.push(i);
-    }else{
+    }else if(i[0]=="$"){
+        secondaryArguments.push(i.slice(1));
+    } else{
         filenames.push(i);
     }
 }
-// //comand to read file
-// if(flag.length==0){
-//     for(let file of filenames){
-//         console.log(fs.readFileSync(file,"utf-8"));
-//     }
-// }else{
-//     //(-rs) command to remove spaces
-//     for(let flag of flags){
-//         if(flag=="-rs"){
-//             for(let file of filenames){
-//                 let filedata=fs.readFileSync(file,"utf-8");
-//                 console.log(filedata.split(" ").join(""));
-//             }
-//         }
-//     }
-// }
-
-
-
-// optimised code 
+ 
 function removeall(string,removaldata){
     return string.split(removaldata).join("");
 }
@@ -41,6 +26,7 @@ for(let file of filenames){
     let filedata=fs.readFileSync(file,"utf-8");
     for(let flag of flags){
 
+        
         //remove space
         if(flag=="-rs"){
             
@@ -50,20 +36,20 @@ for(let file of filenames){
         if(flag=="-rn"){
             filedata=removeall(filedata,"\r\n");
         }
+        //remove special characters
         if(flag=="-rsc"){
-            
+            for( let secondaryArgument of secondaryArguments){
+                filedata= removeall(filedata,secondaryArgument)
+            }            
         }
         //Add sequence numbering with every line
         if(flag=="-s"){
-            let data=addSequence(filedata);
-            console.log(data);
-
+            filedata=addSequence(filedata);
         }
 
         //Add sequence only on non empty line
         if(flag=="-sn"){
-            let data=addSequenceTnel(filedata);
-            console.log(data);
+            filedata=addSequenceTnel(filedata);
         }
         //remove more then one new line
         if(flag=="-rel"){
@@ -71,11 +57,14 @@ for(let file of filenames){
             for(let i=0;i<ans.length;i++){
                 console.log(ans[i]);
             }
+            
         }
         //copy folder 1 content to folder 2
         //search on google CAT COMMANTS IN LINUX
     }
-    console.log(filedata);
+    console.log(filedata);        
+    
+    
 }
 
 
@@ -83,7 +72,7 @@ function addSequence(content){
     let contentArr=content.split("\r\n");
     let count=1;
     for(let i=0;i<contentArr.length;i++){
-        contentArr[i]=(i+1)+" "+contentArr[i];
+        contentArr[i]=(i+1)+". "+contentArr[i];
     }
     return contentArr;
 }
@@ -100,8 +89,9 @@ function addSequenceTnel(content){
     
     return contentArr;
 }
-function removeExtraLine(content){
-    let contentArr=content.split("\r\n")
+
+function removeExtraLine(fileData){
+    let contentArr=fileData.split("\r\n");
     let data=[];
     for(let i=1;i<contentArr.length;i++){
         if(contentArr[i]=="" && contentArr[i-1]==""){
@@ -110,18 +100,13 @@ function removeExtraLine(content){
         if(contentArr[i]=="" && contentArr[i-1]==null){
             contentArr[i]=null;
         }
-
     }
+
     for(let i=0;i<contentArr.length;i++){
         if(contentArr[i]!=null){
             data.push(contentArr[i]);
         }
-
     }
     return data;
-
-
 }
-
-
 
